adde373f6c4d6a03db8450b6d4a2dd044035cdf0
unknown <@ngknn.local>
2023-09-19 14:50:41 +0300

    init commit

---
+++ app/src/main/java/com/example/myapplication/screen_first_enter.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class screen_first_enter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen_first_enter)
+    }
+}
