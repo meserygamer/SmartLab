1e8cf6fc1096d15157798560e789bad92668a30f
Mesery <Meserygamer@gmail.com>
2023-09-22 18:29:50 +0300

    Сделан переход со SplashScreen

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, screen_first_enter::class.java)
+            startActivity(intent)
+        }, 3000)
     }
 }
