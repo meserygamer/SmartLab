a1d0bda79c2394b3bbe02441ac34e0b2b560caef
MeseryGG <Meserygamer@gmail.com>
2023-11-27 21:58:20 +0300

    Окно оплаты готово

--- app/src/main/java/com/example/myapplication/payment.kt
+++ app/src/main/java/com/example/myapplication/payment.kt
@@ -3,7 +3,12 @@ package com.example.myapplication
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.example.myapplication.databinding.ActivityPaymentBinding
+import java.util.Timer
+import java.util.TimerTask
 
 class payment : AppCompatActivity() {
 
@@ -12,10 +17,55 @@ class payment : AppCompatActivity() {
     public val binding : ActivityPaymentBinding
         get() = _binding!!
 
+    var timer : Timer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         _binding = ActivityPaymentBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        (binding.animation.background as AnimationDrawable).start()
+        var animation : Animation = AnimationUtils.loadAnimation(this, R.anim.payment_animation)
+        animation.repeatCount = Animation.INFINITE
+        binding.animation.startAnimation(animation)
+        timer = Timer();
+        timer!!.schedule(object : TimerTask() {
+
+            override fun run() {
+
+                runOnUiThread(object : Runnable{
+
+                    override fun run() {
+
+                        binding.PaymentStatusTextView.text = "Производим оплату...";
+
+                    }
+
+                })
+            }
+
+        }, 3000)
+        timer!!.schedule(object : TimerTask() {
+
+            override fun run() {
+
+                runOnUiThread(object : Runnable{
+
+                    override fun run() {
+
+                        finishPayment()
+
+                    }
+
+                })
+            }
+
+        }, 6000)
     }
+
+    private fun finishPayment()
+    {
+        binding.PaymentAnimationLinearLayout.visibility = View.GONE;
+        binding.SuccessfullPaymentLinearLayout.visibility = View.VISIBLE;
+    }
+
 }
